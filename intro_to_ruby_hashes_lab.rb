e24cec4985c2505305691a64254e366a53889aaf
John Hill <john.m.hill91@gmail.com>
2019-03-27 23:33:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -56,7 +56,7 @@ monopoly = {
 	      :four_pieces_owned => 200
 	    },
 	    :names => {
-	      :reading_railroad => {},
+	      :reading_railroad => { "mortgage_value" => '$100'},
 	      :pennsylvania_railroad => {},
 	      :b_and_o_railroad => {},
 	      :shortline => {}
